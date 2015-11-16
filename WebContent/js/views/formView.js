b7df557b55d983c51ceb81d924d41cbe41e8bb2c
unknown <kessmgr@WNJ0034ALDVSASV.uswin.ad.vzwcorp.com>
2015-11-16 17:37:53 -0500

    Reformat & added dynamic log section logic

--- WebContent/js/views/formView.js
+++ WebContent/js/views/formView.js
@@ -11,41 +11,13 @@ define([
 
 		events: {
 			"click .btn-add": "addSection",
-			"click .btn-remove": "removeSection"
+			"click .btn-remove": "removeSection",
+			"click .btn-submit": "buildForm",
+			"click .btn-cancel": "cancelSection"
 		},
 
 		initialize: function() {
 			this.logIndex = 0;
-			this.nameValidators = {
-				validators: {
-					notEmpty: {
-						message: 'Log name is required'
-					}
-				}
-			};
-			this.serversValidators = {
-				validators: {
-					notEmpty: {
-						message: 'Server list where logs live is required'
-					},
-					isbn: {
-						message: 'Servers not valid'
-					}
-				}
-			};
-			this.sizeValidators = {
-				validators: {
-					notEmpty: {
-						message: 'Log size is required'
-					},
-					numeric: {
-						message: 'Log size must be a numeric number'
-					}
-				}
-			};
-			this.reqValidators = {
-				validators: {}
-			};
 		},
 
 		render: function() {
@@ -54,8 +26,7 @@ define([
 			this.$el.html(formTemplate);
 		},
 
-		addSection: function() {
-			console.log("Adding form section!");
+		addSection: function(e) {
 			this.logIndex++;
 			var $template = $('#logTemplate'),
 				$clone = $template
@@ -70,34 +41,38 @@ define([
 				.find('[name="name"]').attr('name', 'log[' + this.logIndex + '].name').end()
 				.find('[name="servers"]').attr('name', 'log[' + this.logIndex + '].servers').end()
 				.find('[name="size"]').attr('name', 'log[' + this.logIndex + '].size').end()
-				.find('[name="req"]').attr('name', 'log[' + this.logIndex + '].req').end();
+				.find('[name="req"]').attr('name', 'log[' + this.logIndex + '].req').end()
 
-			// Add new fields
-			// Note that we also pass the validator rules for new field as the third parameter
-			// $('#onb-form')
-			// 	.formValidation('addField', 'log[' + this.logIndex + '].name', this.nameValidators)
-			// 	.formValidation('addField', 'log[' + this.logIndex + '].servers', this.serversValidators)
-			// 	.formValidation('addField', 'log[' + this.logIndex + '].size', this.sizeValidators)
-			// 	.formValidation('addField', 'log[' + this.logIndex + '].req', this.reqValidators);
+			var $btn = $(e.currentTarget);
+
+			$btn
+				.removeClass('btn-primary btn-add')
+				.addClass('btn-danger btn-remove')
+				.html('Remove Log');
+
+			var $cancelBtn = $(e.currentTarget).closest('div').children('span:last');
+			console.log($cancelBtn.text())
+			if($cancelBtn.text().indexOf('Cancel') >= 0) {
+				$cancelBtn.remove();
+			}
 		},
 
 		removeSection: function(e) {
 			console.log("Removing form section!");
-			var $row = $(this.el).closest('.row');
+			var $row = $(e.currentTarget).closest('div');
 
-			console.log($(this.el));
-			console.log($row);
-
-			// Remove fields
-            // $('#bookForm')
-            //     .formValidation('removeField', $row.find('[name="log[' + index + '].name"]'))
-            //     .formValidation('removeField', $row.find('[name="log[' + index + '].servers"]'))
-            //     .formValidation('removeField', $row.find('[name="log[' + index + '].size"]'))
-            //     .formValidation('removeField', $row.find('[name="log[' + index + '].req"]'))
-
-			// Remove element containing the fields
 			$row.remove();
+		},
 
+		cancelSection: function(e) {
+			console.log("Cancelling Section");
+			var x = $(e.currentTarget).closest('div');
+			// this.removeSection(e);
+			console.log(x);
+		},
+
+		buildForm: function() {
+			console.log("This is the form being built to submit!");
 		}
 	});
 
